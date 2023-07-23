fa1a93c430c4edb993d353a79b0b367aa98eb2da
PREChairman <51071483+PREChairman@users.noreply.github.com>
2023-07-23 22:29:18 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 /** 更多样式 http://developer.baidu.com/map/custom/list.htm */
-var MAP_STYLE = "hardedge";
+var MAP_STYLE = "googlelite";
 
 var DATA = {
 	"city_1" : {
@@ -16,9 +16,9 @@ var SPEC_POS = {
 	"univ_C" : [121.597479, 31.185356]
 };
 
-var MAP_TITLE = "毕业去向";
+var MAP_TITLE = "毕业蹭饭图";
 
 var ABOUT = {
-	"作者" : ["Yuehao"],
+	"作者" : ["MisakaSKywalker"],
 	"框架" : ["<a href='https://github.com/yuehaowang/irmap' target='_blank'>irmap</a>", "Bootstrap", "百度地图"]
 };
